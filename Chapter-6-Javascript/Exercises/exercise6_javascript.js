6a16dd2cdb0d6bbe4a04d6255304ca16ae9a50c0
snowyyfare8 <snowytejada08@gmail.com>
2026-04-28 12:04:16 +0300

    added reset cost function

--- Chapter-6-Javascript/Exercises/exercise6_javascript.js
+++ Chapter-6-Javascript/Exercises/exercise6_javascript.js
@@ -3,5 +3,12 @@ function calculateTotal () {
     let c = Number(document.getElementById("cost").value); // Cost of petrol per litre, default 1.72
     let n = Number(document.getElementById("number").value); // Number of litres purchased
     let total = c * n; 
-    document.getElementById("total").innerText = total;
+    document.getElementById("total").innerText = total
+}
+
+// Function to reset total cost 
+function resetCost () {
+
+    document.getElementById("total").innerText = "0";
+
 }
